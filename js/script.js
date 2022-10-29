a34391573152407e6a7d8ec6c7ebc8ede1fdb845
mateus040 <mateusgabrielmoreno264@gmail.com>
2022-10-29 16:43:52 -0300

    Adicionando o proejto (finalizado)

---
+++ js/script.js
@@ -0,0 +1,24 @@
+const container = document.querySelector('.container'),
+qrInput = container.querySelector('.form input'),
+generateBtn = container.querySelector('.form button'),
+qrImg = container.querySelector('.qr-code .img');
+
+generateBtn.addEventListener('click', () =>{
+    let qrValue = qrInput.value;
+    if (!qrValue){
+        alert('Insira uma URL ou texto para gerar um QR Code')
+        return;
+    }
+    generateBtn.innerText = "Gerando um QR Code...";
+    qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=170x170&data=${qrValue}`;
+    qrImg.addEventListener('load', () => {
+        generateBtn.innerText = "Gerar QR Code";
+        container.classList.add('active');
+    });
+});
+
+qrInput.addEventListener('keyup', () => {
+    if (!qrInput.value){
+        container.classList.remove('active');
+    }
+})
